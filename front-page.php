83b44356567d009a4c3c18e240fb72f7dd5a6751
yamaguchi371 <telework-takefu6@outlook.jp>
2020-07-13 10:24:12 +0900

    レイアウト構成
    レイアウト、トップへ戻るボタンを実装

--- front-page.php
+++ front-page.php
@@ -1,8 +1,39 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <div class="row">
-    </div>
-</div>
+<div class="wrapper">
+    <!-- wrapper start ▼ -->
 
+    <div id="main" class="main">
+        <!-- main start ▼ -->
+
+        <section class="section">
+
+            <div class="container">
+                <div class="row">
+                <div class="container-inner">
+                    <h2 class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit officia soluta harum, natus nesciunt fugiat ducimus minus nihil consequuntur? Perspiciatis unde ratione temporibus odit harum molestias atque odio at voluptas.</h2>
+                    <h3 class="text-center">おまかせ</h3>
+                    <p>けんか</p>
+                    <p>喧嘩</p>
+                    <p>ケンカ</p>
+                    <div class="food-menu">
+                        <div class="row">
+                            <div class="col-2 offset-2 mb-3 pl-3">lorem</div>
+                            <div class="col-2 offset-3 mb-3 pl-3">lorem</div>
+                            <div class="col-4 mb-3 pl-3">lorem</div>
+                            <div class="col-4 offset-4 mb-3">lorem</div>
+                            <div class="col-4 mb-3">lorem</div>
+                            <div class="col-4 mb-3">lorem</div>
+                        </div>
+                    </div>
+                    <button class="btn-dark btn-lg">こちら</button>
+                </div><!-- container-inner end -->
+                </div><!-- row end -->
+            </div><!-- container end -->
+
+        </section>
+
+    </div><!-- main end ▲ -->
+<div id="page_top" class="arrow"><a href="#"></a></div>
+</div><!-- wrapper end ▲ -->
 <?php get_footer(); ?>

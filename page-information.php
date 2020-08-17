2fd7627da1cd4ef1db34b32360e766a16f3175f2
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-08-17 13:47:37 +0900

    店舗案内ページ作成

---
+++ page-information.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Template Name: 店舗情報
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container-fluid bg-white">
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 my-5">
+            <div class="inf-box">
+                <h3>店舗情報</h3>
+                <h4 class="inf-title">営業時間</h4>
+                <p>12:00～24:00（L.O.24:00）</p>
+                <h4 class="inf-title">営業日</h4>
+                <p>定休日：日曜日</p>
+                <h4 class="inf-title">座席</h4>
+                <p>大部屋 6～20人、8畳間 4～6人、囲炉裏部屋 4～8人 カウンター 8席</p>
+                <h4 class="inf-title">駐車場</h4>
+                <p>10台</p>
+                <h4 class="inf-title">禁煙/喫煙</h4>
+                <p>屋内原則禁煙</p>
+                <h4 class="inf-title">最寄り駅</h4>
+                <p>福井鉄道福武線 ／ 商工会議所前駅 徒歩4分（300m）<br>福井鉄道福武線 ／ 赤十字前駅 徒歩4分（320m）<br>福井鉄道福武線 ／ 足羽山公園口駅 徒歩10分（760m）</p>
+                <div class="inf-btn">
+                    <button class="button-square centering">
+                        <span>ご予約はこちら</span><span>▶</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>    
+
+<?php get_footer(); ?>

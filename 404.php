ad58baa2c0b48234ab0dc12a45d854f6ceeb1c77
yamaguchi371 <telework-takefu6@outlook.jp>
2020-10-08 15:37:36 +0900

    10/8修正

--- 404.php
+++ 404.php
@@ -7,6 +7,6 @@
 http://<?php echo esc_html($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']); ?>
 </span></p>
 <p>現在表示する記事がありません。</p>
-<p><a href="<?php echo home_url(); ?>">トップページへ戻る</a></p> 
+<p><a href="<?php echo esc_url(home_url('/')); ?>">トップページへ戻る</a></p> 
 </div>
 <?php get_footer(); ?>
